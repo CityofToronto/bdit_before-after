4c15318f2d801bff6ffbf373e9930f7bcd3f10a4
Raphael Dumas <raphael.a.dumas@gmail.com>
2016-11-18 17:23:44 -0500

    Created Schema for before after
    Attempt at normalization
    Inserting signal_retiming data

---
+++ signal_retiming/insert_signal_retiming_fromstaging.sql
@@ -0,0 +1,26 @@
+﻿/*
+author: Raphael Dumas
+Takes unique values from the signal retiming staging table and inserts them into their respective tables before inserting the signal retiming periods.
+Then joins all these tables together to get the respective ids, splits the dates text field into a daterange and inserts into the intervention_periods main table
+*/
+
+INSERT INTO beforeafter.intervention_type (intervention)
+VALUES ('signal retiming');
+
+INSERT INTO beforeafter.routes (route_name)
+SELECT DISTINCT route_name FROM beforeafter.signal_retiming_staging;
+
+INSERT INTO beforeafter.segments (segment_name)
+SELECT DISTINCT segment_name FROM beforeafter.signal_retiming_staging;
+
+WITH date_split AS(SELECT route_name, segment_name, string_to_array(period_text, ' to ') datearray, note
+  FROM beforeafter.signal_retiming_staging) 
+
+INSERT INTO beforeafter.intervention_periods(
+            intervention_type_id, route_id, segment_id, 
+            period, note)
+SELECT intervention_type_id, route_id, segment_id, daterange(to_date(datearray[1], 'Month DD, YYYY'), to_date(COALESCE(datearray[2],datearray[1]), 'Month DD, YYYY'), '[]') as period , note
+  FROM date_split
+  INNER JOIN beforeafter.intervention_type ON intervention = 'signal retiming'
+  INNER JOIN beforeafter.routes USING (route_name)
+  INNER JOIN beforeafter.segments USING (segment_name)
---
+++ sql/before_after_schema.sql
@@ -0,0 +1,48 @@
+﻿
+
+DROP TABLE IF EXISTS beforeafter.intervention_periods ;
+CREATE TABLE beforeafter.intervention_periods (
+	intervention_id smallserial PRIMARY KEY,
+	intervention_type_id smallint not null,
+	route_id smallint not null, 
+	segment_id smallint not null,
+	period daterange not null,
+	note varchar(256)
+);
+
+
+DROP TABLE IF EXISTS beforeafter.intervention_type;
+
+CREATE TABLE beforeafter.intervention_type(
+	intervention_type_id smallserial PRIMARY KEY,
+	intervention varchar(32) NOT NULL UNIQUE
+);
+
+DROP TABLE IF EXISTS beforeafter.routes;
+
+CREATE TABLE beforeafter.routes (
+	route_id smallserial PRIMARY KEY,
+	route_name varchar(36) not null UNIQUE
+);
+
+DROP TABLE IF EXISTS beforeafter.segments;
+
+CREATE TABLE beforeafter.segments (
+	segment_id smallserial PRIMARY KEY,
+	segment_name varchar(64) not null UNIQUE
+);
+
+DROP TABLE IF EXISTS beforeafter.segment_tmcs;
+CREATE TABLE beforeafter.segment_tmcs(
+	segment_id smallint not null,
+	tmc char(9) NOT NULL,
+	PRIMARY KEY(segment_id, tmc)
+);
+
+DROP TABLE IF EXISTS beforeafter.signal_retiming_staging;
+CREATE TABLE beforeafter.signal_retiming_staging(
+	route_name varchar(36) not null,
+	segment_name varchar(64) not null,
+	period_text varchar(64) not null,
+	note varchar(256)
+);
